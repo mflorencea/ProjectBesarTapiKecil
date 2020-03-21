13c270a5902f97257167a3542a3e0281bc565957
Michelle <michelleflorence28@gmail.com>
2020-03-21 10:50:38 +0700

    this is my project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { FormsModule } from "@angular/forms"; // <--
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { HeaderComponent } from "./header/header.component";
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, AboutComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
